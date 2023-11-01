b1143b36c8d2d7e794d4edd155c9b785cc59101c
bahamonde <bahamondealine@gmail.com>
2023-11-01 02:52:32 -0300

    tentando subir no cyclic 11

--- infraestrutura/conexao.js
+++ infraestrutura/conexao.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql'); //chamando a dependencia do mysql
 
 const conexao = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
+    host: "mysql://root:4GFegdDAGhF36F-Gf23F5DFfedcHGGG-@monorail.proxy.rlwy.net:21438/railway",
+    port: 3000,
     user: "root",
     password: '',
     database: 'alunos',

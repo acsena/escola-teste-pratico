29c6cb2164af8e27bca7f8eb487e53b1fa71baed
bahamonde <bahamondealine@gmail.com>
2023-11-05 06:20:15 -0300

    testando mysql remoto

--- infraestrutura/conexao.js
+++ infraestrutura/conexao.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql'); //chamando a dependencia do mysql
 
 const conexao = mysql.createConnection({
-    host: process.env.DATABASE_URL,
-    port: process.env.PORT,
+    host: "viaduct.proxy.rlwy.net",
+    port: 16812,
     user: "root",
-    password: '',
-    database: 'alunos',
+    password: '-3BCcCe6bhHcDF-3FGB25Ada6ed5BbD4',
+    database: 'railway',
 });
 
 module.exports = conexao; //exportar a conexao com o banco
